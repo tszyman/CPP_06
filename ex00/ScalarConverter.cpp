6bc0c97b6ab399303eb368458099434fd72b7d67
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2026-03-08 18:23:15 +0100

    [ex00] initial files added

---
+++ ex00/ScalarConverter.cpp
@@ -0,0 +1,20 @@
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter() {
+	
+}
+
+ScalarConverter::ScalarConverter(const ScalarConverter& src) {
+	*this = src;
+}
+
+ScalarConverter& ScalarConverter::operator=(const ScalarConverter& rhs) {
+	if (this != &rhs) {
+		
+	}
+	return *this;
+}
+
+ScalarConverter::~ScalarConverter() {
+	
+}
---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,15 @@
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+class ScalarConverter {
+public:
+	ScalarConverter();											// Default constructor
+	ScalarConverter(const ScalarConverter& src);				// Copy constructor
+	ScalarConverter& operator=(const ScalarConverter& rhs);	// Copy assignment operator
+	~ScalarConverter();											// Destructor
+
+private:
+	
+};
+
+#endif // SCALARCONVERTER_HPP
